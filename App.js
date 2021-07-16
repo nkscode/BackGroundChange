9ceb4e036549435f54473fc4dc1f869b8edc5e21
Nitin Singh <nitinsingh@Nitins-MacBook-Air.local>
2021-07-16 14:51:31 +0530

    Used Prettier for formatting

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
 import {
-  Text, TouchableOpacity, StyleSheet,View, StatusBar
-} from "react-native";
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  StatusBar,
+} from 'react-native';
 
 //const randomColor = "rgb(32,0,126)"
 
-
-
 const App = () => {
-
-  const [randomColor, setRandomColor] = useState("rgb(32,0,126)");
+  const [randomColor, setRandomColor] = useState('rgb(32,0,126)');
 
   const changedBG = () => {
-    let color = 'rgb('+
+    let color =
+      'rgb(' +
       Math.floor(Math.random() * 256) +
       ',' +
       Math.floor(Math.random() * 256) +
@@ -20,24 +22,20 @@ const App = () => {
       Math.floor(Math.random() * 256) +
       ')';
 
-      setRandomColor(color);
-  }
+    setRandomColor(color);
+  };
 
-return (
-<>
+  return (
+    <>
+      <StatusBar backgroundColor={randomColor} />
 
-<StatusBar backgroundColor={randomColor}/>
-
-
-<View style = {[styles.container,{ backgroundColor : randomColor} ]} >
-<TouchableOpacity onPress ={changedBG}>
-<Text style = {styles.text}> Tap Me </Text>
-</TouchableOpacity>
-
-</View>
-</>
-
-);
+      <View style={[styles.container, {backgroundColor: randomColor}]}>
+        <TouchableOpacity onPress={changedBG}>
+          <Text style={styles.text}> Tap Me </Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 };
 
 export default App;
@@ -45,17 +43,17 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   text: {
     fontSize: 30,
-    backgroundColor: "#BB2CD9",
+    backgroundColor: '#BB2CD9',
     paddingVertical: 10,
     paddingHorizontal: 40,
     color: '#FFFFFF',
-  
-    textTransform: "uppercase"
-  }
+    borderRadius: 40,
+    textTransform: 'uppercase',
+  },
 });
